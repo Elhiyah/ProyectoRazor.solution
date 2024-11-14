cb619f4cd472b0705e2b19919536cbb3e2626b13
Elhiyah <eliaspol66@gmail.com>
2024-11-14 12:14:19 -0400

    Add project files.

---
+++ Proyecto_Razor/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_Razor.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Proyecto_Razor/Pages/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_Razor.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
